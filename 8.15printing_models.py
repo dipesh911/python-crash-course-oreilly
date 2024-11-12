6fbba469505d64dde76c18ddd8df0ce04d2c7d32
dipesh911 <66004996+dipesh911@users.noreply.github.com>
2024-11-12 21:18:04 +0530

    func modules 8.15

---
+++ 8.15printing_models.py
@@ -0,0 +1,6 @@
+import printing_functions8_15
+unprinted_designs = ['phone case', 'robot pendant', 'dodecahedron']
+completed_models = []
+
+printing_functions8_15.print_models(unprinted_designs, completed_models)
+printing_functions8_15.show_completed_models(completed_models)
